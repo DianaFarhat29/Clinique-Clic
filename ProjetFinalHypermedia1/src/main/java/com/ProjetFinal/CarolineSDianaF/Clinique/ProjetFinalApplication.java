6100b2996afb53dc2aa5a3e669418b0ca450a496
Diana Farhat <Diana.farhat@outlook.com>
2023-11-05 11:33:47 -0500

    Mise à jour

--- ProjetFinalHypermedia1/src/main/java/com/ProjetFinal/CarolineSDianaF/Clinique/ProjetFinalApplication.java
+++ ProjetFinalHypermedia1/src/main/java/com/ProjetFinal/CarolineSDianaF/Clinique/ProjetFinalApplication.java
@@ -1,9 +1,14 @@
-package com.ProjetFinal.CarolineSDianaF.Clinique;
+package com.ProjetFinal.CarolineSDianaF;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+
 
 @SpringBootApplication
+@EnableConfigurationProperties
+@EntityScan(basePackages = {"com.ProjetFinal.CarolineSDianaF.Modeles"})
 public class ProjetFinalApplication {
 
 	public static void main(String[] args) {
